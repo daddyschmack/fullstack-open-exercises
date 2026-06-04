ce286d6e346c5212e0ac89dc1da905e822ff3c92
John Jaeckle <jjaeckle@apexsystems.com>
2026-06-04 15:29:00 -0500

    refactoring course conmponent to it's own module

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,38 +1,8 @@
 import {useState} from 'react'
 import './App.css'
+import Course from './components/course'
 
-const ExercisesTotal = (props) => {
-    const totalExercises = props.parts.reduce((tot, {exercises}) => tot + exercises, 0)
-    return <p><b>Total of {totalExercises} exercises</b></p>
-}
-const Topics = (props) => {
-    const parts = props.parts;
-    return (
-     <div id="topic-list">
-         {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-         <ExercisesTotal parts={parts}/>
-     </div>
-    )
-}
-const Course = (courses) => {
 
-    return (
-        <div>
-            <h1>Web Development Curriculum</h1>
-        <div className="course">
-            {courses.course.map(course => {
-                return (
-                <div key={course.id}>
-                    <h2>{course.name}</h2>
-                   <Topics parts={course.parts}/>
-                </div>
-                )
-                }
-            )}
-        </div>
-        </div>
-    )
-}
 const App = () => {
     const courses = [
         {
