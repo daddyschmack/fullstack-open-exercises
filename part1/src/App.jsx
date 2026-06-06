33e0fa1bb895eb604f545749bf44225cc4570d43
John Jaeckle <jjaeckle@apexsystems.com>
2026-06-06 08:54:15 -0500

    exercie 2.8: add number to the form and state

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,24 +1,32 @@
 import { useState } from 'react'
 
 const App = () => {
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState(null)
   const addPerson = (event) =>{
       event.preventDefault()
       // We already have the current input value in the newName state!
-      setPersons( persons.concat({name: newName}))
+      setPersons( persons.concat({name: newName, number: newNumber}))
 
       setNewName('');
+      setNewNumber('');
   }
   const handleNameChange = (event) => {
       const newName = event.target.value;
-      let isDuplicate = persons.filter( person => person.name === newName)
-      if(isDuplicate.length > 0){
+      let isDuplicate = persons.some( person => person.name === newName)
+      if(isDuplicate){
           return alert(`${newName} is already added to phonebook`)
       }
       setNewName(newName)
+  }
+  const handleNumberChange = (event) => {
+      const number = event.target.value
+      setNewNumber(number)
+
   }
 
   return (
@@ -28,13 +36,15 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
-
+        <div>
+            number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+        {persons.map(person => <p key={person.name}>{person.name}-{person.number}</p>)}
     </div>
   )
 }
