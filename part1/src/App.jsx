45121a11c328673ba5d23f2521ae5127ed0b0eeb
John Jaeckle <jjaeckle@apexsystems.com>
2026-06-09 13:07:55 -0500

    2.11, getting data from the server, and formatting

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import PersonsForm from './components/PersonsForm'
+import axios from "axios";
+import './App.css'
 
 const Filter = ({filter, setFilter})=> {
     const handleSearch = (event) => {
@@ -14,24 +16,25 @@ const Filter = ({filter, setFilter})=> {
 const Persons = (props) => {
     const personsToShow = props.persons.filter( person => person.name.toLowerCase().includes(props.filter.toLowerCase()))
     return (
-        <ul>
+        <ul className="personList">
             {personsToShow.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
         </ul>
     )
 }
 const App = () => {
+    let [persons, setPersons] = useState([]);
+    const hook = () => {
+        axios.get('http://localhost:3001/persons')
+            .then((response) => {
+                setPersons(response.data)
+            })
+    }
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
-
+  useEffect(hook, [])
   const addPerson = (event) =>{
       event.preventDefault()
         let isDuplicate = persons.some( person => person.name === newName)
